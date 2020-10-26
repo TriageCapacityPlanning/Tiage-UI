89c88418c9caecdd0bb48409a547989e520007b0
KristieTom12 <kristietom12@gmail.com>
2020-10-26 01:14:50 -0400

    feat(common layout & prediction ui): simple ui layout of navbar and menu

---
+++ src/app/get-prediction/main/get-prediction.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { PredictionResults } from '../types';
+
+@Component({
+  selector: 'app-get-prediction',
+  templateUrl: './get-prediction.component.html',
+  styleUrls: ['./get-prediction.component.css']
+})
+export class GetPredictionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/get-prediction/user-input/get-prediction-user-input.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GetPredictionUserInputComponent } from './get-prediction-user-input.component';
+
+describe('UserProfileComponent', () => {
+  let component: GetPredictionUserInputComponent;
+  let fixture: ComponentFixture<GetPredictionUserInputComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GetPredictionUserInputComponent ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GetPredictionUserInputComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
