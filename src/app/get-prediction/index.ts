d0c298d1fb0b7c6bb6562897c9bda6e64598586e
KristieTom12 <kristietom12@gmail.com>
2020-10-26 01:14:50 -0400

    feat(common layout & prediction ui): simple ui layout of navbar and menu

---
+++ src/app/get-prediction/index.ts
@@ -0,0 +1,3 @@
+export { GetPredictionUserInputComponent } from './user-input/get-prediction-user-input.component';
+export { PredictionResultsComponent } from './prediction-results/prediction-results.component';
+export { GetPredictionComponent } from './main/get-prediction.component'
