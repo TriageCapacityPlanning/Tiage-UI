a578fb540f2e42a3f84a49135e78ca71da4802ed
KristieTom12 <kristietom12@gmail.com>
2020-11-02 02:40:34 -0500

    fix(prediction): support semi-urgent and standard patient results

--- src/app/http-get.service.ts
+++ src/app/http-get.service.ts
@@ -1,27 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
-
-export function parseParams(params: Object) {
-  const queryParams: string[] = [];
-  for (const key in params) {
-    queryParams.push(`${key}=${params[key]}`);
-  }
-  return queryParams.join('&');
-}
-
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
   constructor(private http: HttpClient) { }
 
-  public getPrediction(params: Object) {
-    const endpoint = 'localhost:3000';
-    const queryParams = parseParams(params);
-    const url = `${endpoint}?${queryParams}`;
+  public getPrediction(url: string) {
     return this.http.get(url);
   }
 }
